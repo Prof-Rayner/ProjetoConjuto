3447474ea00d24fb0cfc81b2d434f4db7a9ac5de
GABRIEL BATISTA DOS SANTOS MALDONADO <gabriel.bsmaldonado@senacsp.edu.br>
2025-06-25 17:20:55 -0300

    Modificando a prod

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function insertToDisplay(data){
-    document.querySelector('#display').value += data
+function insertToDisplay(data) {
+    const display = document.querySelector('#display');
+    const currentValue = display.value;
+ 
+    if (data === '.') {
+        // Verifica se o último número já contém um ponto
+        const lastNumber = currentValue.split(/[\+\-\*\/]/).pop();
+        if (lastNumber.includes('.')) {
+            return; // Não insere outro ponto
+        }
+    }
+ 
+    display.value += data;
 }
-
-function clean (){
+function clean(){ 
     document.querySelector('#display').value = ''
 }
 
